17e3bc46ab5243dd920fdb7496c3e1061ee3d597
Tomas Gordyn <tomasgordyn@MacBook-Pro-de-Tomas.local>
2023-07-03 22:05:40 -0300

    Revert "Merge pull request #2 from No-Country/feature/header"
    This reverts commit 93c8d819ca4913dd69140e1a98b6e6cd34c4605d, reversing
    changes made to 7a73dea3b479d4135d07f46759aa71e5d0340add.

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,14 +1,6 @@
-import Header from "../components/Header";
-
 function HomePage() {
 	return (
-		<div>
-			<Header/>
-			<div style={{margin: '20px'}}>
-				<h1>Bienvenido a nuestro e-commerce de ropa vintage!</h1>
-			</div>
-		</div>
-		
+		<h1>Bienvenido a nuestro e-commerce de ropa vintage!</h1>
 	);
 }
 

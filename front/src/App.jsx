e414d8da22702586a9c3a70f6eb90806bdac77f6
Tomas Gordyn <tomasgordyn@MacBook-Pro-de-Tomas.local>
2023-07-04 17:39:58 -0300

    integracion de sign up con Header

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,11 +2,15 @@ import './App.css';
 import Router from "../src/utils/Router";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { ToastContainer } from "react-toastify";
 
 
 function App() {
   return (
+    <>
     <Router/>
+    <ToastContainer />
+    </>
   )
 }
 

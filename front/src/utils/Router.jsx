018a07a369c0166378725cf0ba11d71d22dec6d3
Artem <lokomotivspb@gmail.com>
2023-07-09 13:29:50 -0400

    feat: Cart

--- front/src/utils/Router.jsx
+++ front/src/utils/Router.jsx
@@ -2,14 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
+import Cart from "../pages/Cart";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 function Router() {
   return (
+    <Provider store={store}>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
+    </Provider>
   );
 }
 

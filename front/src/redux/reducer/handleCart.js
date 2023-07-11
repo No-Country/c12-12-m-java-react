5d79b68a533fb4a0c9620bb2bcc652568911aa3b
Tomas Gordyn <tomasgordyn@MacBook-Pro-de-Tomas.local>
2023-07-11 09:16:56 -0300

    Agregado componente Under Construction

--- front/src/redux/reducer/handleCart.js
+++ front/src/redux/reducer/handleCart.js
@@ -1,33 +1,39 @@
-const cart = []
+const cart = [];
 
-const handleCart = (state=cart, action) =>{
-    const product = action.payload
-    switch(action.type){
-        case "ADDITEM":
-            // Check if product already in cart
-            const exist = state.find((x) => x.id === product.id)
-            if(exist){
-                // Increase the quantity
-                return state.map((x)=>x.id ===product.id?{...x, qty: x.qty+1}:x)
-            }
-            else{
-                return [...state, {...product, qty:1}]
-            }
-            break;
-        case "DELITEM":
-            const exist2 = state.find((x) => x.id === product.id)
-            if(exist2.qty === 1){
-                return state.filter((x)=>x.id!==exist2.id)
-            }
-            else{
-                return state.map((x)=> x.id===product.id?{...x, qty:x.qty-1}:x)
-            }
-            break;
-
-        default:
-            return state
-            break;
+const handleCart = (state = cart, action) => {
+    const product = action.payload;
+    let updatedState;
+  
+    switch (action.type) {
+      case "ADDITEM": {
+        const exist = state.find((x) => x.id === product.id);
+        if (exist) {
+          updatedState = state.map((x) =>
+            x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+          );
+        } else {
+          updatedState = [...state, { ...product, qty: 1 }];
+        }
+        break;
+      }
+      case "DELITEM": {
+        const exist2 = state.find((x) => x.id === product.id);
+        if (exist2.qty === 1) {
+          updatedState = state.filter((x) => x.id !== exist2.id);
+        } else {
+          updatedState = state.map((x) =>
+            x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+          );
+        }
+        break;
+      }
+      default:
+        updatedState = state;
+        break;
     }
-}
-
-export default handleCart
+  
+    return updatedState;
+  };
+  
+  export default handleCart;
+  

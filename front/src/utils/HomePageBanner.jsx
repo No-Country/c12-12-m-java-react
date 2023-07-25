269d8ea67e75c95656cb7b65fa6c72f653f6d954
tomas gordyn <tomas.gordyn@gmail.com>
2023-07-24 22:32:07 -0300

    Cambios generales esteticos

--- front/src/utils/HomePageBanner.jsx
+++ front/src/utils/HomePageBanner.jsx
@@ -1,25 +1,32 @@
-import { GroupCloth, Shoes, FemaleCloth, MaleCloth, Accesories } from '../assets/Images/Images';
+import {
+  GroupCloth,
+  Shoes,
+  FemaleCloth,
+  MaleCloth,
+  Accesories,
+} from "../assets/Images/Images";
 const data = [
-    {
-        img: GroupCloth,
-        name: "Camperas"
-    },
-    {
-        img: Shoes,
-        name: "Calzados"
-    },
-    {
-        img: FemaleCloth,
-        name: "Camisas-Remeras"
-    },
-    {
-        img: MaleCloth,
-        name: "Pantalones"
-    },
-    {
-        img: Accesories,
-        name: "Accesorios"
-    }
-        
-]
-export default data
+  {
+    img: GroupCloth,
+    name: "Camisas-Remeras",
+  },
+  {
+    img: Shoes,
+    name: "Calzados",
+  },
+
+  {
+    img: MaleCloth,
+    name: "Pantalones",
+  },
+  {
+    img: Accesories,
+    name: "Accesorios",
+  },
+
+  {
+    img: FemaleCloth,
+    name: "Sombreros",
+  },
+];
+export default data;
